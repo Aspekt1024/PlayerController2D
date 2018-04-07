1edc6acb7de142a38a9b652ea5da9e85787932cd
Dan-PC <d.szabo87@gmail.com>
2018-04-08 03:05:37 +1000

    Bounds check now correctly positions the player in both x and y directions

--- Assets/Scripts/Player/PlayerComponents/PlayerBoundsCheck.cs
+++ Assets/Scripts/Player/PlayerComponents/PlayerBoundsCheck.cs
@@ -8,23 +8,46 @@ namespace Aspekt.PlayerController
     {
         private Player player;
         private Rigidbody2D body;
-        private float colliderExtentY;
+        private Collider2D coll;
 
         public PlayerBoundsCheck(Player player, Rigidbody2D body)
         {
             this.player = player;
             this.body = body;
-            colliderExtentY = player.GetComponent<Collider2D>().bounds.extents.y;
+            coll = player.GetComponent<Collider2D>();
         }
 
         public void CheckBounds(float deltaTime)
         {
             RaycastHit2D hit = Physics2D.Raycast(player.transform.position, body.velocity, body.velocity.magnitude * Time.fixedDeltaTime, 1 << LayerMask.NameToLayer("Terrain"));
 
+            bool positionCorrected = false;
+            float xVelocity = body.velocity.x;
+            float yVelocity = body.velocity.y;
+            float xPos = body.position.x;
+            float yPos = body.position.y;
+
             if (hit.collider != null)
             {
-                player.transform.position = hit.point - Mathf.Sign(body.velocity.y) * colliderExtentY * Vector2.up;
-                body.velocity = Vector2.zero;
+                if (Mathf.Abs(hit.point.y - body.position.y) > coll.bounds.extents.y)
+                {
+                    yPos = hit.point.y - Mathf.Sign(yVelocity) * coll.bounds.extents.y;
+                    positionCorrected = true;
+                    yVelocity = 0f;
+                }
+
+                if (Mathf.Abs(hit.point.x - body.position.x) > coll.bounds.extents.x)
+                {
+                    xPos = hit.point.x - Mathf.Sign(xVelocity) * coll.bounds.extents.x;
+                    positionCorrected = true;
+                    xVelocity = 0f;
+                }
+
+                if (positionCorrected)
+                {
+                    body.position = new Vector2(xPos, yPos);
+                    body.velocity = new Vector2(xVelocity, yVelocity);
+                }
             }
         }
     }
